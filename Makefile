2a3495825b8422cb4a866c422c3f9e1be017341a
BSaaber <maxim-movshin@yandex.ru>
2023-05-22 19:00:35 +0300

    .

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: TCPEchoClient TCPEchoServer
-TCPEchoClient: TCPEchoClient.c DieWithError.c
+all: client server skameika
+client: TCPEchoClient.c DieWithError.c
 	gcc TCPEchoClient.c DieWithError.c -o client
-TCPEchoServer: TCPEchoServer.c DieWithError.c
+server: TCPEchoServer.c DieWithError.c
 	gcc TCPEchoServer.c DieWithError.c HandleTCPClient.c -o server
+skameika: TCPEchoSkameika.c DieWithError.c
+	gcc TCPEchoSkameika.c DieWithError.c HandleSkameikaTCPClient.c -o skameika
+
 clean:
-	rm server client
+	rm server client skameika
--- TCPEchoClient.c
+++ TCPEchoClient.c
@@ -12,6 +12,7 @@ void DieWithError(char *errorMessage);  /* Error handling function */
 
 char* success_response =       "okay\0";
 char* error_response =         "fail\0";
+
 char* rent_request =           "rent\0";
 char* free_request =           "free\0";
 char* sleep_hotel_stage =      "slph\0";
@@ -23,31 +24,38 @@ int CLIENTS_AMOUNT = 10;
 
 int main(int argc, char *argv[])
 {
-    int sock;                        /* Socket descriptor */
-    struct sockaddr_in echoServAddr; /* Echo server address */
-    unsigned short echoServPort;     /* Echo server port */
-    char *servIP;                    /* Server IP address (dotted quad) */
-    char *echoString;                /* String to send to echo server */
+
+
+
+
     char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
     int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv()
                                         and total bytes read */
 
     srand(time(NULL));
 
-    if (argc != 5)    /* Test for correct number of arguments */
+    if (argc != 7)    /* Test for correct number of arguments */
     {
-        fprintf(stderr, "Usage: %s <Server IP> <Echo Port> <Clients amount> <Day length (seconds)>\n",
+        fprintf(stderr, "Usage: %s <Hotel server IP> <Echo port> <Clients amount> <Day length (seconds)> <Skameika server IP> <Echo port>\n",
                 argv[0]);
         exit(1);
     }
 
-    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
-
-    echoServPort = atoi(argv[2]); /* Use given port, if any */
 
     CLIENTS_AMOUNT = atoi(argv[3]);
 
     DAY_LENGTH = atoi(argv[4]);
+
+    //------------------  Create Hotel connection  --------------------------
+
+    int sock;                        /* Socket descriptor */
+    struct sockaddr_in echoServAddr; /* Echo server address */
+    unsigned short echoServPort;     /* Echo server port */
+    char *servIP;                    /* Server IP address (dotted quad) */
+
+    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
+    echoServPort = atoi(argv[2]); /* Use given port, if any */
+
 
     /* Create a reliable, stream socket using TCP */
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
@@ -63,6 +71,36 @@ int main(int argc, char *argv[])
     if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
         DieWithError("connect() failed");
 
+    //------------------------------------------------------------------------
+
+
+
+    //------------------  Create Skameika connection  ------------------------
+
+    int sock_skameika;                        /* Socket descriptor */
+    struct sockaddr_in echoServAddr_skameika; /* Echo server address */
+    unsigned short echoServPort_skameika;     /* Echo server port */
+    char *servIP_skameika;                    /* Server IP address (dotted quad) */
+
+    servIP_skameika = argv[5];             /* First arg: server IP address (dotted quad) */
+    echoServPort_skameika = atoi(argv[6]); /* Use given port, if any */
+
+
+    /* Create a reliable, stream socket using TCP */
+    if ((sock_skameika = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        DieWithError("socket() failed");
+
+    /* Construct the server address structure */
+    memset(&echoServAddr_skameika, 0, sizeof(echoServAddr_skameika));     /* Zero out structure */
+    echoServAddr_skameika.sin_family      = AF_INET;             /* Internet address family */
+    echoServAddr_skameika.sin_addr.s_addr = inet_addr(servIP_skameika);   /* Server IP address */
+    echoServAddr_skameika.sin_port        = htons(echoServPort_skameika); /* Server port */
+
+    /* Establish the connection to the echo server */
+    if (connect(sock, (struct sockaddr *) &echoServAddr_skameika, sizeof(echoServAddr_skameika)) < 0)
+        DieWithError("connect() failed");
+
+    //------------------------------------------------------------------------
 
     char* clients[CLIENTS_AMOUNT];
     int clients_wait_time[CLIENTS_AMOUNT];
@@ -123,7 +161,31 @@ int main(int argc, char *argv[])
                 }
             } else if (!strcmp(clients[i], sit_request)) {
                 printf("client goes to skameika\n");
-                // todo - request to skameika server
+
+                if (send(sock_skameika, clients[i], strlen(clients[i]), 0) != strlen(clients[i]))
+                    DieWithError("send() sent a different number of bytes than expected");
+
+                totalBytesRcvd = 0;
+                while (totalBytesRcvd < MESSAGE_SIZE) {
+                    /* Receive up to the buffer size (minus 1 to leave space for
+                    a null terminator) bytes from the sender */
+                    if ((bytesRcvd = recv(sock_skameika, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
+                        DieWithError("recv() failed or connection closed prematurely");
+                    totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
+                    echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
+                    printf("message, that client %d recieved: ", i);
+                    printf("%s\n", echoBuffer);      /* Print the echo buffer */
+
+                    // case success - client rent a room
+                    if (strcmp(echoBuffer, success_response)) {
+                        printf("WHAT? I CAN NOT SIT ON SKAMEIKA? THIS IS STRANGE!\n");
+                        exit(-1);
+                    }
+                    printf("i got to the skameika.\n");
+                }
+
+
+
                 clients[i] = sleep_skameika_stage;
                 clients_wait_time[i] = 1 + rand() % 4; //  sleep from 1 to 5 * DAY_LENGTH seconds (+ process time)
                 printf("I am at skameika now. I go to sleep on skameika for %d days\n", clients_wait_time[i]);
@@ -145,7 +207,7 @@ int main(int argc, char *argv[])
                     printf("%s\n", echoBuffer);      /* Print the echo buffer */
 
                     // case success - client rent a room
-                    if (!strcmp(echoBuffer, error_response)) {
+                    if (strcmp(echoBuffer, success_response)) {
                         printf("WHAT? I CAN NOT FREE A ROOM? THIS IS STRANGE!\n");
                         exit(-1);
                     }
