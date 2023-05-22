f33c9d5fc50d648db16e3e7d1302d2db41e3d476
BSaaber <maxim-movshin@yandex.ru>
2023-05-22 16:50:29 +0300

    first commit

---
+++ HandleTCPClient.c
@@ -0,0 +1,49 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for recv() and send() */
+#include <unistd.h>     /* for close() */
+#include <string.h>
+
+#define RCVBUFSIZE 32   /* Size of receive buffer */
+
+void DieWithError(char *errorMessage);  /* Error handling function */
+
+void HandleTCPClient(int clntSocket, int* free_space)
+{
+    char echoBuffer[RCVBUFSIZE];        /* Buffer for echo string */
+    int recvMsgSize;                    /* Size of received message */
+    char* success_response = "success\0";
+    char* error_response = "error\0";
+    char* rent_request = "rent\0";
+    char* free_request = "free\0";
+    char* response;
+
+    /* Receive message from client */
+    if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
+        DieWithError("recv() failed");
+
+    /* Send received string and receive again until end of transmission */
+    while (recvMsgSize > 0)      /* zero indicates end of transmission */
+    {
+        if (!strcmp(free_request, echoBuffer)) {
+            *free_space++;
+            response = success_response;
+        } else if (!strcmp(rent_request, echoBuffer)) {
+            if (*free_space > 0) {
+                *free_space--;
+                response = success_response;
+            } else {
+                response = error_response;
+            }
+        }
+        /* Echo message back to client */
+        if (send(clntSocket, response, strlen(response), 0) != strlen(response))
+            DieWithError("send() failed");
+
+        /* See if there is more data to receive */
+        if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
+            DieWithError("recv() failed - more data to recv");
+    }
+
+    close(clntSocket);    /* Close client socket */
+}
+
